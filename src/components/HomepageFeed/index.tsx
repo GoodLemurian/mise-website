ff5a09f998710a0fafc0c85407647528d4443d7d
GoodLemurian <pormuerte@gmail.com>
2025-04-15 00:29:51 +0900

    fix path

--- src/components/HomepageFeed/index.tsx
+++ src/components/HomepageFeed/index.tsx
@@ -6,7 +6,7 @@ function BlogFeedComponent() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const url = 'https://chugokusyoten.jp/blog/feed.json';
+    const url = './blog/feed.json';
 
     fetch(url)
       .then((response) => {
